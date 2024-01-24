8526f0a575921c0ee860e571f30ea933f0bf245b
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-24 11:37:42 -0300

    first commit

---
+++ src/main/java/com/gabriel/authenticatebackend/AuthenticateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.authenticatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateBackendApplication.class, args);
+	}
+
+}
