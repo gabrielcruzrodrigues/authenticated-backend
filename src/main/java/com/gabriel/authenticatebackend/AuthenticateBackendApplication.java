bda11b4de9a1c473895feb8d55b7c0ae2d2db950
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-27 13:53:32 -0300

    finishing CommandLineRunner

--- src/main/java/com/gabriel/authenticatebackend/AuthenticateBackendApplication.java
+++ src/main/java/com/gabriel/authenticatebackend/AuthenticateBackendApplication.java
@@ -1,7 +1,17 @@
 package com.gabriel.authenticatebackend;
 
+import com.gabriel.authenticatebackend.model.ApplicationUser;
+import com.gabriel.authenticatebackend.model.Role;
+import com.gabriel.authenticatebackend.repository.RoleRepository;
+import com.gabriel.authenticatebackend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class AuthenticateBackendApplication {
@@ -10,4 +20,21 @@ public class AuthenticateBackendApplication {
 		SpringApplication.run(AuthenticateBackendApplication.class, args);
 	}
 
+	//creating Admin user whenever the application starts
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1, "admin", passwordEncoder.encode("password"), roles);
+			userRepository.save(admin);
+		};
+
+
+	}
 }
