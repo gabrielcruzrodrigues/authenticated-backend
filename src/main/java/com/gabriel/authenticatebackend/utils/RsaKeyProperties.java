d8013ba80df757d19554555408c4ad356c08ee31
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2024-01-31 10:37:36 -0300

    created package with method for create Keys Rsa

---
+++ src/main/java/com/gabriel/authenticatebackend/utils/RsaKeyProperties.java
@@ -0,0 +1,36 @@
+package com.gabriel.authenticatebackend.utils;
+
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+public class RsaKeyProperties {
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public RsaKeyProperties() throws IllegalAccessException {
+        KeyPair keyPair = KeyGeneratorUtility.generateRsaKey();
+        this.publicKey = (RSAPublicKey) keyPair.getPublic();
+        this.privateKey = (RSAPrivateKey) keyPair.getPublic();
+    }
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(RSAPublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(RSAPrivateKey privateKey) {
+        this.privateKey = privateKey;
+    }
+}
